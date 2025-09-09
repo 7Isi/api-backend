2adb2392be8813b148a2666e8fb1a8f6ad6eb535
7Isi <missclic.m7@gmail.com>
2025-09-09 11:28:14 -0300

    09.09.2025

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,13 @@
+// src/users/dto/create-user.dto.ts
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8, { message: 'A senha deve ter pelo menos 8 caracteres' })
+  password: string;
+}
